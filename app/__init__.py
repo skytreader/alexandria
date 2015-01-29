e94a3a9db999ba08aa89da5e2c315ab3e0846652
Chad Estioco <chadestioco@gmail.com>
2015-01-29 19:55:30 +0800

    Trying to load from template.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("home.jinja")
