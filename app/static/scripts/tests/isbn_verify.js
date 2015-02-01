abe972779126b81fb165b18fe549913ced77aa84
Chad Estioco <chadestioco@gmail.com>
2015-02-02 05:17:37 +0800

    Better unit test reports.

--- app/static/scripts/tests/isbn_verify.js
+++ app/static/scripts/tests/isbn_verify.js
@@ -1,3 +1,3 @@
 QUnit.test("stripExtraneousTest", function(assert){
-    assert.ok(stripExtraneous("3.1  4 1 5 92-653") == "3141592653", "Passed!");
+    assert.equal(stripExtraneous("3.1  4 1 5 92-653"), "3141592653");
 });
