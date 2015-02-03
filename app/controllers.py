f452a9b3e097bb80e392a8308436bb66ff8a01fa
Chad Estioco <chadestioco@gmail.com>
2015-02-04 03:13:37 +0800

    Baby steps recreating the login sequence.

--- app/controllers.py
+++ app/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request
 from forms import LoginForm, SearchForm
 
 librarian = Blueprint('librarian', __name__)
@@ -8,7 +8,20 @@ def index():
     form = SearchForm(request.form)
     return render_template("home.jinja", form=form)
 
-@librarian.route("/login/")
+@librarian.route("/login/", methods=["GET", "POST"])
 def login():
     form = LoginForm(request.form)
+
+    if form.validate_on_submit():
+        user = Librarian.query.filter_by(username=form.username.data).first()
+
+        if user and user.password == form.password.data:
+            session["user_id"] = user.id
+
+
     return render_template("login.jinja", form=form)
+
+@librarian.route("/dashboard/")
+@login_required
+def dashboard():
+    pass
