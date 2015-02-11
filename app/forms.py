eea80306962c50e5b7f7126a684035d1ab793a9a
Chad Estioco <chadestioco@gmail.com>
2015-02-12 04:09:25 +0800

    Blueprint for the add book form.

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,20 @@ class SearchForm(Form):
 class LoginForm(Form):
     username = TextField("Username", [Required(message="Enter your username")])
     password = PasswordField("Password", [Required(message="Enter your password")])
+
+class AddBooks(Form):
+    isbn_message = "ISBN is a book's identifier and can often be found at the book's copyright page or near the barcode."
+    isbn = TextField("ISBN", [Required(message=isbn_message)])
+    title = TextField("Title", [Required(message="Book's Title")])
+    genre = TextField("Genre", [Required(message="Genre")])
+
+    # Hardcode for now, so this makes roles.role_display kinda pointless
+    authors = TextField("Author(s)")
+    illustrators = TextField("Illustrator(s)")
+    editors = TextField("Editor(s)")
+    translators = TextField("Translator(s):")
+
+    publisher = TextField("Publisher", [Required(message="Publisher")])
+    printer = TextField("Printer", [Required(message="Printer")])
+    # TODO Clarify this in models!!!
+    year = TextField("Year", [Required(message="Edition Year")])
