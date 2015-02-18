f228351f9b7fc99bfd42355299060c53ade6556e
Chad Estioco <chadestioco@gmail.com>
2015-02-18 18:53:46 +0800

    Proper import structure for __init__ that registers the api Blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-from api import librarian_api
 from controllers import librarian
 from flask import Flask
 from flask.ext.login import LoginManager
@@ -10,6 +9,7 @@ app.config.from_object("config")
 db = SQLAlchemy(app)
 from models import Librarians
 app.register_blueprint(librarian)
+from api import librarian_api
 app.register_blueprint(librarian_api)
 db.create_all()
 db.session.commit()
--- app/api.py
+++ app/api.py
@@ -1,7 +1,13 @@
+from app import app
 from flask import Blueprint
+from flask.ext.login import login_required
+from models import Books, Genres, BookCompanies, BookPersons
 
 librarian_api = Blueprint("librarian_api", __name__)
 
 @librarian_api.route("/book_adder")
+@login_required
 def book_adder():
     return "Hello there."
+
+app.register_blueprint(librarian_api)
