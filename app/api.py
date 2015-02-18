1860decedbbae5620c1faa5cc4772105b696a91d
Chad Estioco <chadestioco@gmail.com>
2015-02-18 20:09:19 +0800

    Dummy for testing the javascript.

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 from app import app
+from app.forms import AddBooksForm
 from flask import Blueprint
 from flask.ext.login import login_required
 from models import Books, Genres, BookCompanies, BookPersons
@@ -8,6 +9,11 @@ librarian_api = Blueprint("librarian_api", __name__)
 @librarian_api.route("/book_adder")
 @login_required
 def book_adder():
-    return "Hello there."
+    form = AddBooksForm()
 
-app.register_blueprint(librarian_api)
+    if form.validate_on_submit():
+        # TODO add the models here
+
+        return "Accepted", 200
+    
+    return "Error", 400
