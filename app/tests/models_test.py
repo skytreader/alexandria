f216b7456f70478ea7a1d98fc90c753c9708c82f
Chad Estioco <chadestioco@gmail.com>
2015-06-19 04:10:46 +0800

    Starting to test stuff.

---
+++ app/tests/models_test.py
@@ -0,0 +1,18 @@
+import os
+import app
+import unittest
+import tempfile
+
+class ModelsTest(unittest.TestCase):
+    
+    def setUp(self):
+        print app.app.config["SQLALCHEMY_TEST_DATABASE_URI"]
+
+    def test_shallow(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
