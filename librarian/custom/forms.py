290856192b214a37812936ae6df5655a5bf2af1a
Chad Estioco <chadestioco@gmail.com>
2015-07-07 06:02:25 +0800

    Add custom JSON type.

---
+++ librarian/custom/forms.py
@@ -0,0 +1,20 @@
+from wtforms import HiddenField
+import json
+
+class JsonField(HiddenField):
+    """
+    JsonField is a subclass of HiddenField since you won't really make your users
+    input JSON data directly no? Most likely, the use case is that the value of
+    this field is automagically generated from other fields.
+    """
+
+    def pre_validate(self, form):
+        superv = super(JsonField, self).prevalidate(form)
+        jsonv = True
+
+        try:
+            json.loads(self.raw_data):
+        except ValueError:
+            jsonv = False
+
+        return superv and jsonv
