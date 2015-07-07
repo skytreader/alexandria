34046593d7b91e4219ab6546442e6318a90e8b19
Chad Estioco <chadestioco@gmail.com>
2015-07-08 00:21:01 +0800

    TDD for backend ISBN verification.

---
+++ librarian/tests/utils_test.py
@@ -0,0 +1,16 @@
+import unittest
+import librarian.utils
+
+class IsbnTests(unittest.TestCase):
+    
+    def test_specified(self):
+        isbn10_correct = "0306406152"
+        isbn13_correct = "9780306406157"
+
+        isbn10_incorrect = "0306406155"
+        isbn13_incorrect = "9780306406155"
+
+        self.assertTrue(librarian.utils.isbn_check(isbn10_correct))
+        self.assertTrue(librarian.utils.isbn_check(isbn13_correct))
+        self.assertFalse(librarian.utils.isbn_check(isbn10_incorrect))
+        self.assertFalse(librarian.utils.isbn_check(isbn13_incorrect))
---
+++ librarian/utils.py
@@ -0,0 +1,9 @@
+from librarian.errors import ConstraintError
+
+def isbn_check(isbn):
+    """
+    Checks for the valididty of the given ISBN string and returns a boolean to
+    indicate validity. If the given string is not exactly either of length 10 or
+    13, raise ConstraintError.
+    """
+    return False
