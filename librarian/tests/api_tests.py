cc1e412229c208c9fd53d0223789dc98a27526cb
Chad Estioco <chadestioco@gmail.com>
2015-07-12 23:01:57 +0800

    Trying to test the api.

--- librarian/tests/api_tests.py
+++ librarian/tests/api_tests.py
@@ -1,12 +1,39 @@
+from librarian.tests.fakers import BookFieldsProvider
+from librarian.tests.factories import BookCompanyFactory, GenreFactory
+
 import librarian
 import unittest
 
 class ApiTests(unittest.TestCase):
     
     def setUp(self):
+        self.app = librarian.app.test_client()
         librarian.init_db(librarian.app.config["SQLALCHEMY_TEST_DATABASE_URI"])
 
     def test_book_adder(self):
-        single_everything = {
-        
+        genre_record = GenreFactory()
+        publisher_record = BookCompanyFactory()
+        printer_record = BookCompanyFactory()
+
+        book_provider = BookFieldsProvider()
+        single_author = {
+            "isbn": book_provider.isbn(),
+            "title": "The Carpet Makers",
+            "genre": genre_record.genre_name,
+            "authors": """[
+                {
+                    "last_name": "Eschenbach",
+                    "first_name": "Andreas"
+                }
+            ]""",
+            "illustrators": "[]",
+            "editors": "[]",
+            "translators": "[]",
+            "publisher": publisher_record.company_name,
+            "printer": printer_record.company_name,
+            "year": "2013"
         }
+
+        single_rv = self.app.post("/api/book_adder", data=single_author)
+
+        self.assertEquals(single_rv._status_code, 200)
