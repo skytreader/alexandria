56a48ddb907d0b000bce8e0420cc920f66c0a2de
Chad Estioco <chadestioco@gmail.com>
2015-07-22 04:17:23 +0800

    Added a new config variable WTF_CSRF_ENABLED.
    This is to prevent the unit test form from invalidating because of CSRF.
    
    TODO Check if plain `CSRF_ENABLED` is actually doing anything.

--- config.py
+++ config.py
@@ -27,6 +27,7 @@ THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED     = not TESTING
+WTF_CSRF_ENABLED = CSRF_ENABLED
 
 # Use a secure, unique and absolutely secret key for
 # signing the data. 
