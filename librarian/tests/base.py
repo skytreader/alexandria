abead69cc721151f2c9a8df251bc013ead220b43
Chad Estioco <chadestioco@gmail.com>
2015-07-22 05:03:07 +0800

    Correcting usage of  from models.

--- librarian/tests/base.py
+++ librarian/tests/base.py
@@ -1,3 +1,5 @@
+from librarian.models import get_or_create, Librarian, Role
+
 import librarian
 import unittest
 
@@ -9,3 +11,11 @@ class AppTestCase(unittest.TestCase):
     
     def setUp(self):
         self.app = librarian.app.test_client()
+        
+        admin_user = get_or_create(Librarian, username="admin", password="admin", is_user_active=True, can_read=True, can_write=True, can_exec=True)
+        roles = ("Author", "Illustrator", "Editor", "Translator")
+        
+        for r in roles:
+            get_or_create(Role, role_name=r, role_display="%s(s)" % r,
+              creator=admin_user.record_id)
+
