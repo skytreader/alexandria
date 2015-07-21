5ed339c7e99134d61d593c742e070f6156038a50
Chad Estioco <chadestioco@gmail.com>
2015-07-22 04:11:41 +0800

    run.py adjustments because variable name change in config.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import librarian
-from config import APP_HOST, APP_PORT, DEBUG
+from config import APP_HOST, APP_PORT, DEVEL
 
 if __name__ == "__main__":
     librarian.init_db()
     librarian.init_blueprints()
-    librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEBUG)
+    librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
