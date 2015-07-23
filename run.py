fcd12b4f8affde1524d732073cbc9193741992ff
Chad Estioco <chadestioco@gmail.com>
2015-07-24 03:21:17 +0800

    Change the order of the calls on init_blueprints and init_db.
    You need to initialize blueprints first before create_all() can auto create the
    tables.

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import librarian
 from config import APP_HOST, APP_PORT, DEVEL
 
 if __name__ == "__main__":
-    librarian.init_db()
     librarian.init_blueprints()
+    librarian.init_db()
     librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
