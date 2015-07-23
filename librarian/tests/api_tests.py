0d29b1c5acb15cdd7167b145e97df0022177ccd9
Chad Estioco <chadestioco@gmail.com>
2015-07-24 05:24:30 +0800

    Adding more assertions to the test.
    But test is failing becuase factory_boy objects are not generating primary keys!

--- librarian/tests/api_tests.py
+++ librarian/tests/api_tests.py
@@ -1,5 +1,6 @@
 from base import AppTestCase
 from faker import Faker
+from librarian.models import Book
 from librarian.tests.fakers import BookFieldsProvider
 from librarian.tests.factories import BookCompanyFactory, GenreFactory
 
@@ -14,13 +15,19 @@ class ApiTests(AppTestCase):
     def setUp(self):
         super(ApiTests, self).setUp()
     
-    def test_book_adder(self):
+    def test_book_adder_happy(self):
         genre_record = GenreFactory()
         publisher_record = BookCompanyFactory()
         printer_record = BookCompanyFactory()
+        librarian.db.session.commit()
+        isbn = fake.isbn()
+
+        # Check that the book does not exist yet
+        the_carpet_makers = librarian.db.session.query(Book).filter(Book.isbn == isbn).all()
+        self.assertEquals(the_carpet_makers, [])
 
         single_author = {
-            "isbn": fake.isbn(),
+            "isbn": isbn,
             "title": "The Carpet Makers",
             "genre": genre_record.genre_name,
             "authors": """[
@@ -40,3 +47,7 @@ class ApiTests(AppTestCase):
         single_rv = self.app.post("/api/book_adder", data=single_author)
 
         self.assertEquals(single_rv._status_code, 200)
+        the_carpet_makers = librarian.db.session.query(Book).filter(Book.isbn == isbn).first()
+        self.assertEquals(isbn, the_carpet_makers.isbn)
+        self.assertEquals("The Carpet Makers", the_carpet_makers.title)
+        self.assertEquals(genre_record.record_id, the_carpet_makers.genre)
