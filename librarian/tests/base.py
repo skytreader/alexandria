227ad1135de1ccdd8da45498ac0af5fa22a68f57
Chad Estioco <chadestioco@gmail.com>
2015-07-30 03:15:35 +0800

    Trying to fix tests from empty db upwards.
    Now there seems to be a consistent failure on API tests because it is returning
    409.

--- librarian/tests/base.py
+++ librarian/tests/base.py
@@ -1,6 +1,7 @@
 from librarian.models import get_or_create, Librarian, Role
 
 import librarian
+import logging
 import unittest
 
 librarian.app.config["TESTING"] = True
@@ -12,11 +13,24 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = librarian.app.test_client()
         """
-        admin_user = get_or_create(Librarian, will_commit=True, username="admin", password="admin", is_user_active=True, can_read=True, can_write=True, can_exec=True)
+        admin_user = Librarian(username="admin", password="admin",
+          is_user_active=True, can_read=True, can_write=True, can_exec=True)
+        librarian.db.session.add(admin_user)
+        roles = ("Author", "Illustrator", "Editor", "Translator")
+        for r in roles:
+            librarian.db.session.add(Role(role_name=r, role_display="%s(s)"%r,
+              creator=admin_user.record_id))
+
+        logging.info("#######CHAD########## flushing stuff")
+        librarian.db.session.flush()
+        """
+        self.admin_user = get_or_create(Librarian, will_commit=True, username="admin", password="admin", is_user_active=True, can_read=True, can_write=True, can_exec=True)
         roles = ("Author", "Illustrator", "Editor", "Translator")
         
         for r in roles:
             get_or_create(Role, will_commit=True, role_name=r, role_display="%s(s)" % r,
-              creator=admin_user.record_id)
-        """
+              creator=self.admin_user.record_id)
 
+    def tearDown(self):
+        logging.info("#######CHAD########## rollback stuff")
+        librarian.db.session.rollback()
