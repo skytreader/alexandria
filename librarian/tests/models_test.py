9a458392860768372eddc36918249833771b13be
Chad Estioco <chadestioco@gmail.com>
2015-08-10 04:03:56 +0800

    Trying to test get_or_create but having problems.
    The problem is in the creation of objects from factories. In the included test,
    a `Genre` is created with the creator field submitted as a username (as opposed
    to an ID). This is where the docs conflict.
    
    The docs for factory_boy: http://factoryboy.readthedocs.org/en/latest/recipes.html#dependent-objects-foreignkey
    and http://factoryboy.readthedocs.org/en/latest/orms.html#sqlalchemy
    
    The id PK is declared among the fields of the factory. However, if I do that in
    the present code base, the id will be passed as a parameter to the ORM object
    constructors, which causes an error.
    
    The models in this code is patterned after
    https://pythonhosted.org/Flask-SQLAlchemy/models.html.

--- librarian/tests/models_test.py
+++ librarian/tests/models_test.py
@@ -1,7 +1,7 @@
 from base import AppTestCase
 from factories import *
 from librarian.errors import ConstraintError
-from librarian.models import ISBN_START
+from librarian.models import Book, get_or_create, ISBN_START
 
 import librarian
 import unittest
@@ -15,3 +15,14 @@ class ModelsTest(AppTestCase):
         with self.assertRaises(ConstraintError):
             # This is an invalid ISBN. It should be 978-3-16-148410-0
             BookFactory(isbn="9783161484105")
+
+    def test_get_and_create(self):
+        templar = librarian.db.session.query(Book).filter(Book.title=="Templar").all()
+        self.assertEqual([], templar)
+        
+        templar_book = BookFactory(title="Templar")
+        librarian.db.session.flush()
+
+        templar = librarian.db.session.query(Book).filter(Book.title=="Templar").all()
+
+        self.assertEqual(1, len(templar))
