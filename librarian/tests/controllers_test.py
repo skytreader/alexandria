0783a5870da3ce3d4ccb60895e56ce6489caa37b
Chad Estioco <chadestioco@gmail.com>
2015-08-19 03:16:19 +0800

    More tests.

--- librarian/tests/controllers_test.py
+++ librarian/tests/controllers_test.py
@@ -8,5 +8,11 @@ class ControllersTest(AppTestCase):
     def testLogin(self):
         successful_data = {"librarian_username": "admin", "librarian_password": "admin"}
         
-        success_case = self.client.post("/login", data=successful_data)
+        success_case = self.client.post("/login/", data=successful_data)
         self.assertEqual(302, success_case.status_code)
+
+        nonexistent_data = {"librarian_username": "nimda", "librarian_password": "nimda"}
+
+        fail_case = self.client.post("/login/", data=nonexistent_data)
+        # Should just render the login page with flash messages.
+        self.assertEqual(200, fail_case.status_code)
