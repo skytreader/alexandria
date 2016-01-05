f6d5a0e6db398f6bb188d0d2b8862569a5a40f1f
Chad Estioco <chadestioco@gmail.com>
2016-01-06 02:18:52 +0800

    Add manual test cleanup to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TEST_DATABASE_URI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -39,5 +39,23 @@ def destroy_db_tables():
     engine.execute("DROP TABLE IF EXISTS book_participants;")
     engine.execute("DROP TABLE IF EXISTS printers;")
     engine.execute("DROP TABLE IF EXISTS pseudonyms;")
+    engine.execute("SET FOREIGN_KEY_CHECKS = 1;")
+    session.commit()
+
+def manual_test_cleanup():
+    engine = create_engine(SQLALCHEMY_TEST_DATABASE_URI)
+    session = sessionmaker(bind=engine)()
+    engine.execute("SET FOREIGN_KEY_CHECKS = 0;")
+    engine.execute("DELETE FROM alembic_version;")
+    engine.execute("DELETE FROM librarians;")
+    engine.execute("DELETE FROM roles;")
+    engine.execute("DELETE FROM genres;")
+    engine.execute("DELETE FROM books;")
+    engine.execute("DELETE FROM book_companies;")
+    engine.execute("DELETE FROM imprints;")
+    engine.execute("DELETE FROM book_persons;")
+    engine.execute("DELETE FROM book_participants;")
+    engine.execute("DELETE FROM printers;")
+    engine.execute("DELETE FROM pseudonyms;")
     engine.execute("SET FOREIGN_KEY_CHECKS = 1;")
     session.commit()
