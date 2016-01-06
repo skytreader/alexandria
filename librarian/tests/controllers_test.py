805451b2d77b5098b30837829c8ee0533303cd0c
Chad Estioco <chadestioco@gmail.com>
2016-01-07 00:40:48 +0800

    Add tests for the new redirect behavior.

--- librarian/tests/controllers_test.py
+++ librarian/tests/controllers_test.py
@@ -5,7 +5,7 @@ class ControllersTest(AppTestCase):
     def setUp(self):
         super(ControllersTest, self).setUp()
 
-    def testLogin(self):
+    def ntest_login(self):
         successful_data = {"librarian_username": "admin", "librarian_password": "admin"}
         
         success_case = self.client.post("/login/", data=successful_data)
@@ -16,3 +16,12 @@ class ControllersTest(AppTestCase):
         fail_case = self.client.post("/login/", data=nonexistent_data)
         # Should just render the login page with flash messages.
         self.assertEqual(200, fail_case.status_code)
+    
+    def test_logged_in_redirect(self):
+        successful_data = {"librarian_username": "admin", "librarian_password": "admin"}
+
+        success_case = self.client.post("/login/", data=successful_data)
+        self.assertEqual(302, success_case.status_code)
+
+        visit_redirect = self.client.get("/login/")
+        self.assertEqual(302, visit_redirect.status_code)
