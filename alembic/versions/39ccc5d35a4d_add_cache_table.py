34093bf1dee5a2727d676d5500a7ab370ac025ab
Chad Estioco <chadestioco@gmail.com>
2016-02-14 04:43:49 +0800

    Initial commit for adding a cache table.
    This will solve our issue with the pagination of book listing results.

---
+++ alembic/versions/39ccc5d35a4d_add_cache_table.py
@@ -0,0 +1,31 @@
+"""add cache table
+
+Revision ID: 39ccc5d35a4d
+Revises: 1748bdf1a3b2
+Create Date: 2016-02-14 04:35:39.345094
+
+The purpose of the cache table is to have a single table aggregating all the
+default data we want to show when listing all books. All FKs so there should
+be no problems with stale data.
+
+This also adds the 'is_preset' column to the roles table.
+"""
+
+# revision identifiers, used by Alembic.
+revision = '39ccc5d35a4d'
+down_revision = '1748bdf1a3b2'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column("roles",
+      sa.Column("is_preset", sa.Boolean(), server_default=False)
+    )
+
+
+def downgrade():
+    op.drop_column("roles", "is_preset")
