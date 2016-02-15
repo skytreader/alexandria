1589bf512d2f0a412f20098d889f39d64fdd240c
Chad Estioco <chadestioco@gmail.com>
2016-02-16 02:48:13 +0800

    Aaaaaannnnnndddd....reformat!

--- alembic/versions/1ac819f296cc_change_default_to_server_default.py
+++ alembic/versions/1ac819f296cc_change_default_to_server_default.py
@@ -24,99 +24,61 @@ def upgrade():
     op.alter_column("librarians", "can_exec", server_default=False)
     op.alter_column("librarians", "is_user_active", server_default=False)
 
-    op.alter_column("genres", "date_created",
-      server_default=None)
-    op.alter_column("genres", "date_modified",
-      server_default=None)
+    op.alter_column("genres", "date_created", server_default=None)
+    op.alter_column("genres", "date_modified", server_default=None)
 
-    op.alter_column("books", "date_created",
-      server_default=None)
-    op.alter_column("books", "date_modified",
-      server_default=None)
+    op.alter_column("books", "date_created", server_default=None)
+    op.alter_column("books", "date_modified", server_default=None)
 
-    op.alter_column("book_companies", "date_created",
-      server_default=None)
-    op.alter_column("book_companies", "date_modified",
-      server_default=None)
+    op.alter_column("book_companies", "date_created", server_default=None)
+    op.alter_column("book_companies", "date_modified", server_default=None)
 
-    op.alter_column("imprints", "date_created",
-      server_default=None)
-    op.alter_column("imprints", "date_modified",
-      server_default=None)
+    op.alter_column("imprints", "date_created", server_default=None)
+    op.alter_column("imprints", "date_modified", server_default=None)
 
-    op.alter_column("book_persons", "date_created",
-      server_default=None)
-    op.alter_column("book_persons", "date_modified",
-      server_default=None)
+    op.alter_column("book_persons", "date_created", server_default=None)
+    op.alter_column("book_persons", "date_modified", server_default=None)
 
-    op.alter_column("roles", "date_created",
-      server_default=None)
-    op.alter_column("roles", "date_modified",
-      server_default=None)
+    op.alter_column("roles", "date_created", server_default=None)
+    op.alter_column("roles", "date_modified", server_default=None)
 
-    op.alter_column("book_participants", "date_created",
-      server_default=None)
-    op.alter_column("book_participants", "date_modified",
-      server_default=None)
+    op.alter_column("book_participants", "date_created", server_default=None)
+    op.alter_column("book_participants", "date_modified", server_default=None)
 
-    op.alter_column("printers", "date_created",
-      server_default=None)
-    op.alter_column("printers", "date_modified",
-      server_default=None)
+    op.alter_column("printers", "date_created", server_default=None)
+    op.alter_column("printers", "date_modified", server_default=None)
 
-    op.alter_column("pseudonyms", "date_created",
-      server_default=None)
-    op.alter_column("pseudonyms", "date_modified",
-      server_default=None)
+    op.alter_column("pseudonyms", "date_created", server_default=None)
+    op.alter_column("pseudonyms", "date_modified", server_default=None)
 
 
 def downgrade():
-    op.alter_column("librarians", "date_created",
-      server_default=None)
-    op.alter_column("librarians", "date_modified",
-      server_default=None)
+    op.alter_column("librarians", "date_created", server_default=None)
+    op.alter_column("librarians", "date_modified", server_default=None)
 
-    op.alter_column("genres", "date_created",
-      server_default=None)
-    op.alter_column("genres", "date_modified",
-      server_default=None)
+    op.alter_column("genres", "date_created", server_default=None)
+    op.alter_column("genres", "date_modified", server_default=None)
 
-    op.alter_column("books", "date_created",
-      server_default=None)
-    op.alter_column("books", "date_modified",
-      server_default=None)
+    op.alter_column("books", "date_created", server_default=None)
+    op.alter_column("books", "date_modified", server_default=None)
 
-    op.alter_column("book_companies", "date_created",
-      server_default=None)
-    op.alter_column("book_companies", "date_modified",
-      server_default=None)
+    op.alter_column("book_companies", "date_created", server_default=None)
+    op.alter_column("book_companies", "date_modified", server_default=None)
 
-    op.alter_column("imprints", "date_created",
-      server_default=None)
-    op.alter_column("imprints", "date_modified",
-      server_default=None)
+    op.alter_column("imprints", "date_created", server_default=None)
+    op.alter_column("imprints", "date_modified", server_default=None)
 
-    op.alter_column("book_persons", "date_created",
-      server_default=None)
-    op.alter_column("book_persons", "date_modified",
-      server_default=None)
+    op.alter_column("book_persons", "date_created", server_default=None)
+    op.alter_column("book_persons", "date_modified", server_default=None)
 
-    op.alter_column("roles", "date_created",
-      server_default=None)
-    op.alter_column("roles", "date_modified",
-      server_default=None)
+    op.alter_column("roles", "date_created", server_default=None)
+    op.alter_column("roles", "date_modified", server_default=None)
 
-    op.alter_column("book_participants", "date_created",
-      server_default=None)
-    op.alter_column("book_participants", "date_modified",
-      server_default=None)
+    op.alter_column("book_participants", "date_created", server_default=None)
+    op.alter_column("book_participants", "date_modified", server_default=None)
 
-    op.alter_column("printers", "date_created",
-      server_default=None)
-    op.alter_column("printers", "date_modified",
-      server_default=None)
+    op.alter_column("printers", "date_created", server_default=None)
+    op.alter_column("printers", "date_modified", server_default=None)
 
-    op.alter_column("pseudonyms", "date_created",
-      server_default=None)
-    op.alter_column("pseudonyms", "date_modified",
-      server_default=None)
+    op.alter_column("pseudonyms", "date_created", server_default=None)
+    op.alter_column("pseudonyms", "date_modified", server_default=None)
