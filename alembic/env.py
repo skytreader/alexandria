fd8777fefe1d51a929dd4ec2209f4cdafc500292
Chad Estioco <chadestioco@gmail.com>
2016-03-08 04:50:41 +0800

    More unsuccessful wrestling.

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,10 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+import sys
+
+sys.path.append("../librarian")
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,7 +20,8 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 import librarian.models
-target_metadata = None
+from librarian.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
