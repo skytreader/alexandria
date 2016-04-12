dcc9bbc3abd2333de2bea570a80541fc4b629b0b
Chad Estioco <chadestioco@gmail.com>
2016-04-12 22:14:59 +0800

    Add migration to rename fields as in 778f73351b11c65db94bb7734b7aff2a490de79f.

---
+++ alembic/versions/4f88e34f90c6_standardize_db_conventions.py
@@ -0,0 +1,89 @@
+"""standardize db conventions
+
+Revision ID: 4f88e34f90c6
+Revises: 1ac819f296cc
+Create Date: 2016-04-12 17:49:41.968930
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4f88e34f90c6'
+down_revision = '1ac819f296cc'
+branch_labels = None
+depends_on = None
+
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    # General creator -> creator_id
+    op.alter_column("librarians", "creator", new_column_name="creator_id")
+    op.alter_column("roles", "creator", new_column_name="creator_id")
+    op.alter_column("genres", "creator", new_column_name="creator_id")
+    op.alter_column("books", "creator", new_column_name="creator_id")
+    op.alter_column("book_companies", "creator", new_column_name="creator_id")
+    op.alter_column("imprints", "creator", new_column_name="creator_id")
+    op.alter_column("book_persons", "creator", new_column_name="creator_id")
+    op.alter_column("book_participants", "creator", new_column_name="creator_id")
+    op.alter_column("printers", "creator", new_column_name="creator_id")
+    op.alter_column("pseudonyms", "creator", new_column_name="creator_id")
+
+    # Genral last_modifier -> last_modifier_id
+    op.alter_column("librarians", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("roles", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("genres", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("books", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("book_companies", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("imprints", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("book_persons", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("book_pariticipants", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("printers", "last_modifier", new_column_name="last_modifier_id")
+    op.alter_column("pseudonyms", "last_modifier", new_column_name="last_modifier_id")
+
+    # Modifications for Book table
+    op.alter_column("books", "genre", new_column_name="genre_id")
+    op.alter_column("books", "publisher", new_column_name="publisher_id")
+
+    # Modifications for Imprint table
+    op.alter_column("imprint", "mother_company", new_column_name="mother_company_id")
+    op.alter_column("imprint", "imprint_company", new_column_name="imprint_company_id")
+
+
+def downgrade():
+    # General creator_id -> creator
+    op.alter_column("librarians", "creator_id", new_column_name="creator")
+    op.alter_column("roles", "creator_id", new_column_name="creator")
+    op.alter_column("genres", "creator_id", new_column_name="creator")
+    op.alter_column("books", "creator_id", new_column_name="creator")
+    op.alter_column("book_companies", "creator_id", new_column_name="creator")
+    op.alter_column("imprints", "creator_id", new_column_name="creator")
+    op.alter_column("book_persons", "creator_id", new_column_name="creator")
+    op.alter_column("book_participants", "creator_id", new_column_name="creator")
+    op.alter_column("printers", "creator_id", new_column_name="creator")
+    op.alter_column("pseudonyms", "creator_id", new_column_name="creator")
+
+    # Genral last_modifier -> last_modifier_id
+    op.alter_column("librarians", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("roles", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("genres", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("books", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("book_companies", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("imprints", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("book_persons", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("book_pariticipants", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("printers", "last_modifier_id", new_column_name="last_modifier")
+    op.alter_column("pseudonyms", "last_modifier_id", new_column_name="last_modifier")
+
+    # Modifications for Book table
+    op.alter_column("books", "genre_id", new_column_name="genre")
+    op.alter_column("books", "publisher_id", new_column_name="publisher")
+
+    # Modifications for Imprint table
+    op.alter_column("imprint", "mother_company_id", new_column_name="mother_company")
+    op.alter_column("imprint", "imprint_company_id", new_column_name="imprint_company")
