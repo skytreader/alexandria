857e45245a5e516c5f0d8dbb4e12a3af95291f19
Chad Estioco <chadestioco@gmail.com>
2016-04-27 02:27:30 +0800

    Hmm, alembic still not working, hrrrr.

--- alembic/versions/1ac819f296cc_change_default_to_server_default.py
+++ alembic/versions/1ac819f296cc_change_default_to_server_default.py
@@ -18,66 +18,67 @@ import sys
 sys.path.append(os.getcwd())
 
 from alembic import op
-import librarian
+from librarian import db
 from librarian.models import ISBN_START
 import sqlalchemy as sa
 
 
 def upgrade():
     op.alter_column("librarians", "date_created",
-      server_default=sa.func.current_timestamp(), type_=librarian.db.DateTime(True))
+      #server_default=db.DateTime(True))
+      server_default=sa.func.current_timestamp())
     op.alter_column("librarians", "date_modified",
-      server_default=sa.func.current_timestamp(), type_=librarian.db.DateTime(True))
+      server_default=db.DateTime(True))
     op.alter_column("librarians", "can_read", server_default=db.false())
     op.alter_column("librarians", "can_write", server_default=db.false())
     op.alter_column("librarians", "can_exec", server_default=db.false())
     op.alter_column("librarians", "is_user_active", server_default=db.false())
 
     op.alter_column("genres", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("genres", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("books", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("books", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("books", "publish_year", server_default=ISBN_START)
 
     op.alter_column("book_companies", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("book_companies", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("imprints", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("imprints", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("book_persons", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("book_persons", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("roles", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("roles", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("book_participants", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("book_participants", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("printers", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("printers", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
     op.alter_column("pseudonyms", "date_created",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
     op.alter_column("pseudonyms", "date_modified",
-      server_default=sa.func.current_timestamp())
+      server_default=db.DateTime(True))
 
 
 def downgrade():
