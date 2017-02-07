24165e2785e3d15494af97e6fbe8349107d7713d
Chad Estioco <chadestioco@gmail.com>
2017-02-08 06:32:57 +0800

    Add resilience.

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import librarian
 from config import APP_HOST, APP_PORT, DEVEL
 
+import time
+
 if __name__ == "__main__":
-    librarian.init_blueprints()
-    librarian.init_db()
-    librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
+    for i in range(8):
+        try:
+            librarian.init_blueprints()
+            librarian.init_db()
+            librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
+        except:
+            print "Can't start app, retrying..."
+            time.sleep(3)
