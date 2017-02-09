9a6c6c6ca02912efd709b07899ca68d1d96a880f
Chad Estioco <chadestioco@gmail.com>
2017-02-10 03:33:07 +0800

    I don't know what is happening.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import librarian
 from config import APP_HOST, APP_PORT, DEVEL
 
 import time
+import traceback
 
 if __name__ == "__main__":
     while True:
@@ -9,6 +10,7 @@ if __name__ == "__main__":
             librarian.init_blueprints()
             librarian.init_db()
             librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
-        except:
+        except Exception:
+            traceback.print_exc()
             print "Can't start app, retrying..."
             time.sleep(3)
