f1c1a591a23d1bc9f039fe71215a0629e271bd63
Chad Estioco <chadestioco@gmail.com>
2017-11-06 20:55:34 +0800

    Use exponential backoff when retrying app connections.
    Closes #65.

--- run.py
+++ run.py
@@ -5,14 +5,17 @@ import time
 import traceback
 
 if __name__ == "__main__":
+    exp_backoff = 0
     while True:
         try:
             librarian.init_blueprints()
             librarian.init_db()
             librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
         except (KeyboardInterrupt, SystemExit):
+            print "KeyboardInterrupt or SystemExit"
             raise
         except Exception:
             traceback.print_exc()
             print "Can't start app, retrying..."
-            time.sleep(3)
+            exp_backoff += 1
+            time.sleep(3 ** exp_backoff)
